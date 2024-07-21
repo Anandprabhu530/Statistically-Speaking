1484fc49355cf34bdc7b65f76174ef5261870ad0
Anand Prabhu <anandprabhu530@gmail.com>
2024-07-21 21:16:25 +0530

    Tried Charts

---
+++ app/api/dashboard/route.ts
@@ -0,0 +1,10 @@
+import { result } from "@/utils/actions";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const query_call =
+    await result(`SELECT location, COUNT(*) AS location_count FROM test_data GROUP BY location ORDER BY location_count DESC;
+ 
+`);
+  return NextResponse.json({ message: query_call });
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Chart_Component } from "../components/Chart_Component";
+
+const page = () => {
+  return (
+    <div className="w-full h-screen">
+      <Chart_Component />
+    </div>
+  );
+};
+
+export default page;
